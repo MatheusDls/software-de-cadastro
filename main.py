02495018fe7764fb59fa62f13d97dd629b4e1c88
MatheusDls <matheuslsdk@gmail.com>
2024-01-05 00:16:09 -0300

    Foi criado list_frame2, e bg das frames

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import *
+from tkinter import ttk
 
 # Cria a janela principal
 root = tk.Tk()
@@ -11,26 +12,25 @@ class Application():
         self.tela()  # Configurações da janela
         self.frames_de_tela()  # Criação dos frames na janela
         self.widgets_frame1()  # Criação dos widgets da 1º tela
+        self.lista_frame2() #Criação do treeviewer na 2º tela
         root.mainloop()
 
     def tela(self):
         # Configurações da janela principal
         self.root.title("Software de cadastro")
         self.root.configure(background='#1e3743')
-        self.root.geometry("1600x1080")
+        self.root.geometry("1200x1080")
         self.root.resizable(True, True)
-        self.root.maxsize(width=1920, height=1080)
+        self.root.maxsize(width=1200, height=1080)
         self.root.minsize(width=800, height=600)
 
     def frames_de_tela(self):
         # Criação de dois frames na janela
-        self.frame_1 = Frame(self.root, bd=4,
-                             highlightbackground='#0000FF', highlightthickness=3)
+        self.frame_1 = Frame(self.root, bd=4, bg='#eeeeee', highlightbackground='#0000FF', highlightthickness=3)
         self.frame_1.place(relx=0.01, rely=0.01, relwidth=0.98, relheight=0.48)
 
-        self.frame_2 = Frame(self.root, bd=4,
-                             highlightbackground='#0000FF', highlightthickness=3)
-        self.frame_2.place(relx=0.01, rely=0.5, relwidth=0.98, relheight=0.49)
+        self.frame_2 = Frame(self.root, bd=4, bg='#eeeeee', highlightbackground='#0000FF', highlightthickness=3)
+        self.frame_2.place(relx=0.01, rely=0.4, relwidth=0.98, relheight=0.49)
     
     def widgets_frame1(self):
         ## Criação do botão Incluir 
@@ -43,7 +43,7 @@ class Application():
 
         ## Criação do botão Relatorio 
         self.bt_relatorio = Button(self.frame_1, text="RELATORIO")
-        self.bt_relatorio.place(relx=0.2, rely=0.0, relwidth=0.1, relheight=0.10)
+        self.bt_relatorio.place(relx=0.3, rely=0.0, relwidth=0.1, relheight=0.10)
 
         ## Criação do botão Ajuda 
         self.bt_ajuda = Button(self.frame_1, text="AJUDA")
@@ -53,6 +53,28 @@ class Application():
     def mostrar_label(self):
         novo_label = Label(self.frame_1, text = "Código")
         novo_label.place(relx = 0.0, rely = 0.13)
+
+    def lista_frame2(self):
+        self.listaCli = ttk.Treeview(self.frame_2, height= 3, column=("col1", "col2", "col3", "col4"))
+        self.listaCli.heading("#0", text="")
+        self.listaCli.heading("#1", text="Codigo")
+        self.listaCli.heading("#2", text="Empresa")
+        self.listaCli.heading("#3", text="telefone")
+        self.listaCli.heading("#4", text="Cidade")
+
+        self.listaCli.column("#0", width=1)
+        self.listaCli.column("#1", width=50)
+        self.listaCli.column("#2", width=200)
+        self.listaCli.column("#3", width=125)
+        self.listaCli.column("#4", width=125)
+
+        relx_centered = (1 - 0.96) / 2  # (1 - relwidth) / 2
+        rely_centered = (1 - 0.85) / 2  # (1 - relheight) / 2
+
+        self.listaCli.place(relx=relx_centered, rely=rely_centered, relwidth=0.94, relheight=0.90)
+
+
+
 
 # Instanciação da aplicação
 Application(root)
