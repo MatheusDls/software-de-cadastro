b1501d51dcd87d8a25815e9b18f98729b4352630
MatheusDls <matheuslsdk@gmail.com>
2024-01-06 00:26:33 -0300

    Criando Entry para inserir codigo

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ class Application():
 
         ## Criação do botão Relatorio 
         self.bt_relatorio = Button(self.frame_1, text="RELATORIO")
-        self.bt_relatorio.place(relx=0.3, rely=0.0, relwidth=0.1, relheight=0.10)
+        self.bt_relatorio.place(relx=0.2, rely=0.0, relwidth=0.1, relheight=0.10)
 
         ## Criação do botão Ajuda 
         self.bt_ajuda = Button(self.frame_1, text="AJUDA")
@@ -54,6 +54,10 @@ class Application():
         novo_label = Label(self.frame_1, text = "Código")
         novo_label.place(relx = 0.0, rely = 0.13)
 
+        self.codigo_entry = Entry(self.frame_1)
+        self.codigo_entry.place(relx=0.05, rely=0.14, relwidth=0.05)
+        
+        ## Configuração de colunas na treeviewer
     def lista_frame2(self):
         self.listaCli = ttk.Treeview(self.frame_2, height= 3, column=("col1", "col2", "col3", "col4"))
         self.listaCli.heading("#0", text="")
@@ -62,9 +66,9 @@ class Application():
         self.listaCli.heading("#3", text="telefone")
         self.listaCli.heading("#4", text="Cidade")
 
-        self.listaCli.column("#0", width=1)
+        self.listaCli.column("#0", width=0)
         self.listaCli.column("#1", width=50)
-        self.listaCli.column("#2", width=200)
+        self.listaCli.column("#2", width=100)
         self.listaCli.column("#3", width=125)
         self.listaCli.column("#4", width=125)
 
